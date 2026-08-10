f7874cd4c7160d5bdaba8e17da63af9936151858
Cameron Lee <camlee2108@gmail.com>
2026-08-10 20:07:49 +0800

    Fixed level 1 board values

--- src/game/gameLogic.js
+++ src/game/gameLogic.js
@@ -7,7 +7,7 @@ export const GRID_SIZE = 5;
 // Same "pattern options per level" table as `levelOptions` in DailyVoltorbView.swift.
 // Each pattern is [count2, count3, count0(voltorbs)] for a 5x5 (25-tile) board.
 export const levelOptions = [
-  [[3, 1, 6], [0, 3, 6], [5, 0, 6], [2, 2, 6], [4, 2, 6]],
+  [[3, 1, 6], [0, 3, 6], [5, 0, 6], [2, 2, 6], [4, 1, 6]],
   [[1, 3, 7], [6, 0, 7], [3, 2, 7], [0, 4, 7], [5, 1, 7]],
   [[2, 3, 8], [7, 0, 8], [4, 2, 8], [1, 4, 8], [6, 1, 8]],
   [[3, 3, 8], [0, 5, 8], [8, 0, 10], [5, 2, 10], [2, 4, 10]],
